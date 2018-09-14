e38271aceb678179f9c144fa43053c518f57dab4
Daisho <daishokomiyama@gmail.com>
2018-09-14 10:55:16 -0400

    Adding collision detection

--- js/main.js
+++ js/main.js
@@ -18,12 +18,17 @@ const mainState = {
         spaceKey.onDown.add(this.jump, this);
 
         this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+        this.score = 0;
+        this.labelScore = game.add.text(20, 20, "0", {
+            font: "30px Arial", fill: "#ffffff"
+        });
     },
 
     update: function() {
         if (this.bird.y < 0 || this.bird.y > 490) {
             this.restartGame();
         }
+        game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
     },
 
     jump: function() {
@@ -38,7 +43,7 @@ const mainState = {
         const pipe = game.add.sprite(x, y, 'pipe');
         this.pipes.add(pipe);
         game.physics.arcade.enable(pipe);
-        pipe.body.velocity.x = - 200;
+        pipe.body.velocity.x = -200;
         pipe.checkWorldBounds = true;
         pipe.outOfBoundsKill = true;
     },
@@ -54,6 +59,9 @@ const mainState = {
             if (i != hole && i != hole + 1)
                 this.addOnePipe(400, i * 60 + 10)
         }
+
+        this.score += 1;
+        this.labelScore.text = this.score;
     }
 };
 
